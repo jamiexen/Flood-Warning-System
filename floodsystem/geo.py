abdccba9ed723f73fa4881d9d2183e2a73b0df59
jamiexen <jamie.maxen@gmail.com>
2023-02-02 15:59:51 +0000

    commit including 1C, and tests for 1B and 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -1,9 +1,28 @@
 # Copyright (C) 2018 Garth N. Wells
-#
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
 geographical data.
 
 """
 
+
+from haversine import haversine
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
+
+def stations_by_distance(stations, p):
+  distances = []
+  stations = build_station_list()
+  for station in stations:
+    distances.append((station.name, haversine(station.coord, p)))
+  distances = sorted_by_key(distances, 1)
+  return distances
+    
+def stations_within_radius(stations, centre, r):
+  Y = []
+  X = stations_by_distance(stations, centre)
+  for i in range(len(X)):
+    if X[i][1] < r:
+      Y.append(X[i][0])
+  Y = sorted_by_key(Y, 0)
+  return Y
