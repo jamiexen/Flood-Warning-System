be555f158867a7631e06d5d1e705d8c1b46c3ad8
Harry Chacksfield <hrac3@cam.ac.uk>
2023-02-14 11:48:51 +0000

    Fixed testing for polyfit

--- test_analysis.py
+++ test_analysis.py
@@ -1,13 +1,13 @@
 """Test for plot function"""
 from floodsystem import analysis as analysis
 import datetime
+import pytest
 
 def test_polyfit():
     """Tests that polyfit returns the expected polynomial from a given set of inputs"""
-    poly, shift = analysis.polyfit([datetime.datetime(0,0,1), datetime.datetime(0,0,2), datetime.datetime(0,0,3), datetime.datetime(0,0,4)],
+    poly, shift = analysis.polyfit([datetime(1,1,1), datetime(1,1,2), datetime(1,1,3), datetime(1,1,4)],
     [0,1,1,0], 2)
-    assert(poly(0) == 0)
-    assert(poly(1) == 1)
-    assert(poly(2) == 1)
-    assert(poly(3) == 0)
-    assert(shift == 0)
+    assert(poly(0) == pytest.approx(0, 1e-15))
+    assert(poly(1) == pytest.approx(1, 1e-15))
+    assert(poly(2) == pytest.approx(1, 1e-15))
+    assert(poly(3) == pytest.approx(0, 1e-15))
