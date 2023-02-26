3b3cd8fa7efe3849c8ddd402c344449a9645655a
hrac3 <hrac3@cam.ac.uk>
2023-02-26 23:43:37 +0000

    Modified Task2B to work with object output from stations_level_over_threshold

--- Task2B.py
+++ Task2B.py
@@ -8,7 +8,11 @@ def run():
   stations = build_station_list()
   update_water_levels(stations)
   print(f"stations with a relative water level over {tol}:")
-  print(*stations_level_over_threshold(stations, tol), sep = "\n")
+  station_list = [x[0] for x in stations_level_over_threshold(stations, tol)]
+  output_list = []
+  for i in range(len(station_list)):
+    output_list.append([station_list[i].name, station_list[i].relative_water_level()])
+  print(*output_list, sep = "\n")
   
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
